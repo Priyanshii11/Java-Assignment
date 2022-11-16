48ecb4393e5a7cd38a6706741fff107f71f6014f
Priyanshii <priyanshik222@gmail.com>
2022-11-16 22:54:34 +0530

    fourth commit

--- q45.java
+++ q45.java
@@ -5,20 +5,25 @@ public class q45 {
         System.out.println("enter the income emlpoyee in lakhs");
         Scanner pri=new Scanner(System.in);
         long a=pri.nextInt();
-        float tax,tax1,tax2;
-        if(a<250000){
+        float tax;
+        if(a<250000)
         System.out.println("not valid,income is not less than 2.5L ");
         else if (a<=250000 && a<500000)
-        tax = a*(5/100);
-        System.out.println("tax is " +tax);
-        else if (a<=500000 && a<1000000)
-        tax1=a*(20/100);
-        System.out.println("tax is "+tax1);
+        {
+        tax = a*0.5f;
+        System.out.println("tax is 5% " +tax );
         }
-        else (a>=1000000)
-        tax2=a*(30/100);
-        System.out.println("tax is "+tax2);
-        
+        else if (a<=500000 && a<1000000)
+        {
+        tax = a*0.2f;
+        System.out.println("tax is 20% "+tax);
+        }
+        else
+        {
+        tax=a*0.3f;
+        System.out.println("tax is 30%" +tax);
+        }
+    
      }
     
 }
